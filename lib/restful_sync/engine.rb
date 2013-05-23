134d4880e1a5d98ff25ad2cfdc7f2042f8812845
damien <damien@glyph>
2013-05-23 13:30:02 +0200

    fixed engine bug

--- lib/restful_sync/engine.rb
+++ lib/restful_sync/engine.rb
@@ -2,8 +2,11 @@ module RestfulSync
   class Engine < ::Rails::Engine
     isolate_namespace RestfulSync
 
-    ActiveSupport.on_load :active_record do
-      ActiveRecord::Base.send(:include, RestfulSync::Referrable)
+    # TODO : put referrable in lib, require file instead of 'if defined?'
+    if defined? RestfulSync::Referrable
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.send(:include, RestfulSync::Referrable)
+      end
     end
   end
 end
